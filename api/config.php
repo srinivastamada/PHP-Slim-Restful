0fc36e5bc8cc2c1ecb74e695dfd5c6862851f53d
Srinivas Tamada <srinivas@9lessons.info>
2017-06-13 15:59:02 -0400

    First Commit

---
+++ api/config.php
@@ -0,0 +1,37 @@
+<?php
+//ob_start("ob_gzhandler");
+error_reporting(0);
+session_start();
+
+/* DATABASE CONFIGURATION */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'banana');
+define("BASE_URL", "http://localhost/PHP-Slim-Restful/api/");
+define("SITE_KEY", 'yourSecretKey');
+
+
+function getDB() 
+{
+	$dbhost=DB_SERVER;
+	$dbuser=DB_USERNAME;
+	$dbpass=DB_PASSWORD;
+	$dbname=DB_DATABASE;
+	$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
+	$dbConnection->exec("set names utf8");
+	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $dbConnection;
+}
+/* DATABASE CONFIGURATION END */
+
+/* API key encryption */
+function apiKey($session_uid)
+{
+$key=md5(SITE_KEY.$session_uid);
+return hash('sha256', $key);
+}
+
+
+
+?>
---
+++ api/index.php
@@ -0,0 +1,126 @@
+<?php
+require 'config.php';
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+$app = new \Slim\Slim();
+
+$app->post('/login','login'); /* User login */
+$app->post('/signup','signup'); /* User Signup  */
+
+//$app->post('/userDetails','userDetails'); /* User Details */
+
+$app->run();
+
+/************************* USER LOGIN *************************************/
+/* ### User login ### */
+function login() {
+    
+    $request = \Slim\Slim::getInstance()->request();
+    $data = json_decode($request->getBody());
+    
+    try {
+        
+        $db = getDB();
+        $userData ='';
+        $sql = "SELECT user_id, name, email, username FROM users WHERE (username=:username or email=:username) and password=:password ";
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("username", $data->username, PDO::PARAM_STR);
+        $password=hash('sha256',$data->password);
+        $stmt->bindParam("password", $password, PDO::PARAM_STR);
+        $stmt->execute();
+        $mainCount=$stmt->rowCount();
+        $userData = $stmt->fetch(PDO::FETCH_OBJ);
+        
+        if(!empty($userData))
+        {
+            $user_id=$userData->user_id;
+            $userData->token = apiKey($user_id);
+        }
+        
+        $db = null;
+        echo '{"userData": ' . json_encode($userData) . '}';
+    }
+    catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+
+/* ### User registration ### */
+function signup() {
+    $request = \Slim\Slim::getInstance()->request();
+    $data = json_decode($request->getBody());
+    $email=$data->email;
+    $name=$data->name;
+    $username=$data->username;
+    $password=$data->password;
+    
+    try {
+        
+        $username_check = preg_match('~^[A-Za-z0-9_]{3,20}$~i', $username);
+        $emain_check = preg_match('~^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.([a-zA-Z]{2,4})$~i', $email);
+        $password_check = preg_match('~^[A-Za-z0-9!@#$%^&*()_]{6,20}$~i', $password);
+        
+        
+        if (strlen(trim($username))>0 && strlen(trim($password))>0 && strlen(trim($email))>0 && $emain_check>0 && $username_check>0 && $password_check>0)
+        {
+            $db = getDB();
+            $userData = '';
+            $sql = "SELECT user_id FROM users WHERE username=:username or email=:email";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam("username", $username,PDO::PARAM_STR);
+            $stmt->bindParam("email", $email,PDO::PARAM_STR);
+            $stmt->execute();
+            $mainCount=$stmt->rowCount();
+            $created=time();
+            if($mainCount==0)
+            {
+                
+                /*Inserting user values*/
+                $sql1="INSERT INTO users(username,password,email,name)VALUES(:username,:password,:email,:name)";
+                $stmt1 = $db->prepare($sql1);
+                $stmt1->bindParam("username", $username,PDO::PARAM_STR);
+                $password=hash('sha256',$data->password);
+                $stmt1->bindParam("password", $password,PDO::PARAM_STR);
+                $stmt1->bindParam("email", $email,PDO::PARAM_STR);
+                $stmt1->bindParam("name", $name,PDO::PARAM_STR);
+                $stmt1->execute();
+                
+                $userData=internalUserDetails($email);
+                
+            }
+            
+            $db = null;
+            
+            echo '{"userData": ' . json_encode($userData) . '}';
+        }
+    }
+    catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+}
+
+
+/* ### internal Username Details ### */
+function internalUserDetails($input) {
+    $sql = "SELECT user_id, name, email, username FROM users WHERE username=:input or email=:input";
+    try {
+        $db = getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam("input", $input,PDO::PARAM_STR);
+        $stmt->execute();
+        $usernameDetails = $stmt->fetch(PDO::FETCH_OBJ);
+        $usernameDetails->token = apiKey($usernameDetails->user_id);
+        return $usernameDetails;
+        
+    } catch(PDOException $e) {
+        echo '{"error":{"text":'. $e->getMessage() .'}}';
+    }
+    
+}
+
+
+
+
+?>
